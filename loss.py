337264f43ecda4a30350bbcaea29c13a82128ec9
ved <vedshah.email@gmail.com>
2025-03-24 19:59:43 -0500

    Moved masked softmaxing to the taxonomy class. Utilizing it in the loss function

--- loss.py
+++ loss.py
@@ -8,8 +8,6 @@ from taxonomies import Taxonomy, ORACLE_Taxonomy
 
 
 # Implementation of Weighted Hierarchical Cross Entropy loss function by Villar et. al. 2023 (https://arxiv.org/abs/2312.02266) based on the Hierarchical Cross Entropy loss function by Bertinetto et. al. 2019 (https://arxiv.org/abs/1912.09393)
-
-
 class WHXE_Loss(nn.Module):
 
     def __init__(self, taxonomy:Taxonomy, alpha=0.5):
@@ -54,37 +52,20 @@ class WHXE_Loss(nn.Module):
 
         #TODO: This could also be sped up....potentially.
 
+        # Apply softmax to sets of siblings for the logits in order to get the pseudo conditional probabilities 
+        conditional_probabilities = self.taxonomy.get_conditional_probabilities(logits)
+
         # Class weights of size (N_nodes), ordered in level order traversal
         class_weights = self.get_class_weights(true)
 
-        # Go through set of siblings
-        for mask in self.masks:
-
-            mask = torch.from_numpy(mask)
-
-            # Get the e^logits
-            exps = torch.exp(logits)
-
-            # Multiply (dot product) the e^logits with the mask to maintain just the e^logits values that belong to this mask. All other values will be zeros.
-            masked_exps = torch.multiply(exps, mask)
-
-            # Find the sum of the e^logits values that belong to the mask. Do this for each element in the batch separately. Add a small value to avoid numerical problems with floating point numbers.
-            masked_sums = torch.sum(masked_exps, dim=1, keepdim=True) + epsilon
-
-            # Compute the softmax by dividing the e^logits with the sum (e^logits)
-            softmax = masked_exps/masked_sums
-
-            # (1 - mask) * y_pred gets the logits for all the values not in this mask and zeros out the values in the mask. Add those back so that we can repeat the process for other masks.
-            logits = softmax + ((1 - mask) * logits)
-
         # At this point we have the masked soft maxes i.e. the pseudo probabilities. We can take the log of these values
-        logits = torch.log(logits)
+        log_p = torch.log(conditional_probabilities)
 
         # Weight them by the level at which the corresponding node appears in the hierarchy
-        logits = logits * self.lambda_term
+        log_p = log_p * self.lambda_term
 
         # Weight them by the class weight after using the target_probabilities as indicators. Then sum them up for each batch
-        v1 = torch.sum(class_weights * (logits * true), dim=1)
+        v1 = torch.sum(class_weights * (log_p * true), dim=1)
 
         # Finally, find the mean over all batches. Since we are taking logs of numbers <1 (the pseudo probabilities), we have to multiply by -1 to get a +ve loss value.
         v2 = -1 * torch.mean(v1)

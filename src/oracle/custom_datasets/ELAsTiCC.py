cec7bab566c6b091c7fd84165cad2db08ec93824
ved <vedshah.email@gmail.com>
2025-04-30 22:50:19 -0500

    Implemented much faster data loader

--- src/oracle/custom_datasets/ELAsTiCC.py
+++ src/oracle/custom_datasets/ELAsTiCC.py
@@ -1,33 +1,35 @@
 import io
 import torch
 
+import polars as pl
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 from PIL import Image
-from pathlib import Path
-from datasets import DatasetDict, load_dataset
-from torch.utils.data import DataLoader
+from tqdm import tqdm
 from torch.nn.utils.rnn import pad_sequence
 
-from oracle.architectures import ORACLE1, ORACLE2_lite_swin
-from oracle.taxonomies import ORACLE_Taxonomy
 from oracle.constants import ELAsTiCC_to_Astrophysical_mappings
 
-time_independent_feature_list = ['MWEBV', 'MWEBV_ERR', 'REDSHIFT_HELIO', 'REDSHIFT_HELIO_ERR', 'HOSTGAL_PHOTOZ', 'HOSTGAL_PHOTOZ_ERR', 'HOSTGAL_SPECZ', 'HOSTGAL_SPECZ_ERR', 'HOSTGAL_RA', 'HOSTGAL_DEC', 'HOSTGAL_SNSEP', 'HOSTGAL_ELLIPTICITY', 'HOSTGAL_MAG_u', 'HOSTGAL_MAG_g', 'HOSTGAL_MAG_r', 'HOSTGAL_MAG_i', 'HOSTGAL_MAG_z', 'HOSTGAL_MAG_Y']
-time_dependent_feature_list = ['MJD', 'PHOTFLAG', 'FLUXCAL', 'FLUXCALERR', 'BAND']
-book_keeping_feature_list = ['SNID', 'ELASTICC_class']
+# <----- constant for the dataset ----->
 
-# Path to this file's directory
-here = Path(__file__).resolve().parent
+batch_size = 512
 
-# Go up to the root, then into data/ and then get the parquet file
-parquet_path = str(here.parent.parent.parent / "data" / 'ELAsTiCC' / 'complete.parquet')
+img_height = 256
+img_width = 256
+n_channels = 3
 
 # <----- Hyperparameters for the model.....Unfortunately ----->
 marker_style = 'o'
 marker_size = 50
 linewidth = 0.75
+
+flag_value = -9
+
+# Flag values for missing data of static feature according to elasticc
+missing_data_flags = [-9, -99, -999, -9999, 999]
+
 
 # Mean wavelengths for the LSST pass bands in micrometers
 LSST_passband_to_wavelengths = {
@@ -49,95 +51,155 @@ LSST_passband_wavelengths_to_color = {
     LSST_passband_to_wavelengths['Y']: np.array((255, 0, 127))/255,
 }
 
-max__elasticc_ts_len = 500
+time_independent_feature_list = ['MWEBV', 'MWEBV_ERR', 'REDSHIFT_HELIO', 'REDSHIFT_HELIO_ERR', 'HOSTGAL_PHOTOZ', 'HOSTGAL_PHOTOZ_ERR', 'HOSTGAL_SPECZ', 'HOSTGAL_SPECZ_ERR', 'HOSTGAL_RA', 'HOSTGAL_DEC', 'HOSTGAL_SNSEP', 'HOSTGAL_ELLIPTICITY', 'HOSTGAL_MAG_u', 'HOSTGAL_MAG_g', 'HOSTGAL_MAG_r', 'HOSTGAL_MAG_i', 'HOSTGAL_MAG_z', 'HOSTGAL_MAG_Y']
+time_dependent_feature_list = ['MJD', 'FLUXCAL', 'FLUXCALERR', 'BAND', 'PHOTFLAG']
+book_keeping_feature_list = ['SNID', 'ELASTICC_class']
 
-flag_value = -9
+n_static_features = len(time_independent_feature_list)
+n_ts_features = len(time_dependent_feature_list)
+n_book_keeping_features = len(book_keeping_feature_list)
 
-# Flag values for missing data of static feature according to elasticc
-missing_data_flags = [-9, -99, -999, -9999, 999]
+def truncate_light_curve_fractionally(x_ts, f=None):
 
-def replace_missing_value_flags(examples):
-
-    # Get the number of samples
-    N_samples = len(examples['SNID'])
-
-    for i in range(N_samples):
-
-        # Replace any missing value flag with -9
-        for feature in time_independent_feature_list:
-            if examples[feature][i] in missing_data_flags:
-                examples[feature][i] = flag_value
+    if f == None:
+        # Get a random fraction between 0.1 and 1
+        f = np.random.uniform(0.1, 1.0)
     
-    return examples
+    original_obs_count = x_ts.shape[0]
 
-def mask_off_saturations(examples):
+    # Find the new length of the light curve
+    new_obs_count = int(original_obs_count * f)
+    if new_obs_count < 1:
+        new_obs_count = 1
 
-    # Get the number of samples
-    N_samples = len(examples['SNID'])
+    # Truncate the light curve
+    x_ts = x_ts[:new_obs_count, :]
 
-    for i in range(N_samples):
+    return x_ts
 
-        # Mask off the saturations
-        PHOTFLAG = examples['PHOTFLAG'][i]
-        saturation_mask =  (np.array(PHOTFLAG) & 1024) == 0 
 
-        # Remove the saturations
-        examples['MJD'][i] = np.array(examples['MJD'][i])[saturation_mask]
-        examples['FLUXCAL'][i] = np.array(examples['FLUXCAL'][i])[saturation_mask] 
-        examples['FLUXCALERR'][i] = np.array(examples['FLUXCALERR'][i])[saturation_mask]
-        examples['BAND'][i] = np.array(examples['BAND'][i])[saturation_mask]
-        examples['PHOTFLAG'][i] = np.array(examples['PHOTFLAG'][i])[saturation_mask]
-    
-    return examples
+class ELAsTiCC_LC_Dataset(torch.utils.data.Dataset):
 
-def truncate_lcs_fractionally(examples, fraction=None):
+    def __init__(self, parquet_file_path, include_lc_plots=False, transform=None):
+        super(ELAsTiCC_LC_Dataset, self).__init__()
 
-    # Get the number of samples
-    N_samples = len(examples['SNID'])
+        # Columns to be read from the parquet file
+        self.columns = time_dependent_feature_list + time_independent_feature_list + book_keeping_feature_list
 
-    # Fraction of their original length to augment the light curves to
-    if fraction is None:
-        # Randomly sample a fraction between 0.1 and 1.0
-        fractions_array = np.random.uniform(0.1, 1.0, N_samples)
-    else:
-        # Use the provided fraction
-        fractions_array = np.array([fraction]*N_samples)
+        self.parquet_file_path = parquet_file_path
+        self.parquet_df = pl.read_parquet(self.parquet_file_path, columns=self.columns)
+        self.transform = transform
+        self.include_lc_plots = include_lc_plots
 
-    for i in range(N_samples):
+        self.clean_up_dataset()
+               
 
-        # Apply the fraction limit on the light curve
-        original_obs_count = len(examples['MJD'][i])
+    def __len__(self):
 
-        # Find the new length of the light curve
-        new_obs_count = int(original_obs_count * fractions_array[i])
-        if new_obs_count < 1:
-            new_obs_count = 1
+        return len(self.parquet_df)
+
+    def __getitem__(self, index):
+
+        row = self.parquet_df.row(index, named=True) 
+
+        snid = row['SNID']
+        ELAsTiCC_class_name = row['ELASTICC_class']
+        astrophysical_class = ELAsTiCC_to_Astrophysical_mappings[ELAsTiCC_class_name]
+
+        lc_length = len(row['MJD_clean'])
+
+        time_series_data = np.zeros((lc_length, n_ts_features), dtype=np.float32)
+        for i, feature in enumerate(time_dependent_feature_list):
+            time_series_data[:,i] = np.array(row[f"{feature}_clean"], dtype=np.float32)
+        time_series_data = torch.from_numpy(time_series_data)
+
+        static_data = torch.zeros(n_static_features)
+        for i, feature in enumerate(time_independent_feature_list):
+            static_data[i] = row[feature]
+
+        if self.transform != None:
+            time_series_data = self.transform(time_series_data)
+
+        dictionary = {
+            'ts': time_series_data,
+            'static': static_data,
+            'label': astrophysical_class,
+            'SNID': snid,
+        }
+
+        if self.include_lc_plots:
+            light_curve_plot = self.get_lc_plots(row)
+            dictionary['lc_plot'] = light_curve_plot
         
-        # Truncate the light curve to its new length and apply other transforms
-        examples['MJD'][i] = np.array(examples['MJD'][i])[:new_obs_count] - examples['MJD'][i][0] # Subtract out time of first observation (could be a detection or a non detection)
-        examples['FLUXCAL'][i] =  np.array(examples['FLUXCAL'][i])[:new_obs_count] 
-        examples['FLUXCALERR'][i] = np.array(examples['FLUXCALERR'][i])[:new_obs_count]
-        examples['BAND'][i] = np.array([LSST_passband_to_wavelengths[b] for b in np.array(examples['BAND'][i])[:new_obs_count]]) # Convert the pass band label (ugrizY) to the mean wavelength of the filter
-        examples['PHOTFLAG'][i] = np.where((np.array(examples['PHOTFLAG'][i])[:new_obs_count] & 4096 != 0), 1, 0) # 1 for detections, 0 for non detections
+        return dictionary
     
-    return examples
+    def clean_up_dataset(self):
 
-def add_lc_plots(examples):
+        def remove_saturations_from_series(phot_flag_arr, feature_arr):
+            
+            saturation_mask =  (np.array(phot_flag_arr) & 1024) == 0 
+            feature_arr = np.array(feature_arr)[saturation_mask]
 
-    # Get the number of samples
-    N_samples = len(examples['SNID'])
+            return feature_arr
+        
+        def replace_missing_flags(x):
 
-    lc_plots_array = []
+            if x in missing_data_flags:
+                if type(x) == int:
+                    return int(flag_value)
+                else:
+                    return float(flag_value)
+            else:
+                return x
+            
+        #TODO. Subtract out time
 
-    for i in range(N_samples):
 
-        flux = examples['FLUXCAL'][i]
-        flux_err = examples['FLUXCALERR'][i]
-        jd = examples['MJD'][i]
-        filters = examples['BAND'][i]
+        # Map pass bands to wavelengths
+        print("Replacing band labels with mean wavelengths")
+        self.parquet_df = self.parquet_df.with_columns(
+            pl.col("BAND").map_elements(lambda x: [LSST_passband_to_wavelengths[band] for band in x]).alias("BAND")
+        )
+
+        # Remove the saturations form the time series data. PHOTFLAG is handled later
+        ts_feature_list = [x for x in time_dependent_feature_list if x != "PHOTFLAG"]
+        for feature in ts_feature_list:
+            print(f"Removing saturations from {feature} series...")
+            self.parquet_df = self.parquet_df.with_columns(
+                pl.struct(["PHOTFLAG", feature]).map_elements(lambda x: remove_saturations_from_series(x['PHOTFLAG'], x[feature])).alias(f"{feature}_clean")
+            )
+
+        # Correct the phot flag
+        print(f"Removing saturations from PHOTFLAG series...")
+        self.parquet_df = self.parquet_df.with_columns(
+            pl.col("PHOTFLAG").map_elements(lambda x: remove_saturations_from_series(x, x)).alias("PHOTFLAG_clean")
+        )
+
+        # Replace the missing flags with a single missing value
+        for feature in time_independent_feature_list:
+            print(f"Replacing missing values in {feature} series with single flag value...")
+            self.parquet_df = self.parquet_df.with_columns(
+                pl.col(feature).map_elements(lambda x: replace_missing_flags(x)).alias(f"{feature}_clean")
+            )
+
+
+    def get_lc_plots(self, row):
+
+        # Get the light curve data
+        jd = np.array(row['MJD'])
+        flux = np.array(row['FLUXCAL'])
+        flux_err = np.array(row['FLUXCALERR'])
+        filters = np.array(row['BAND'])
+        phot_flag = np.array(row['PHOTFLAG']) # NOTE: might want to use a different marker for ND
 
         # Create a figure and axes
         fig, ax = plt.subplots(1, 1)
+
+        # Set the figure size in inches
+        width_in = 2.56  # Desired width in inches (256 pixels / 100 dpi)
+        height_in = 2.56  # Desired height in inches (256 pixels / 100 dpi)
+
+        fig.set_size_inches(width_in, height_in)
 
         # Remove all spines (black frame)
         for spine in ax.spines.values():
@@ -146,17 +208,11 @@ def add_lc_plots(examples):
         for wavelength in LSST_passband_wavelengths_to_color.keys():
             
             idx = np.where(filters == wavelength)[0]
-            ax.errorbar(jd[idx], flux[idx], yerr=flux_err[idx], linewidth=linewidth, color=LSST_passband_wavelengths_to_color[wavelength])
-            ax.scatter(jd[idx], flux[idx], marker=marker_style, s=marker_size, color=LSST_passband_wavelengths_to_color[wavelength])
+            ax.errorbar(jd[idx], flux[idx], yerr=flux_err[idx], linewidth=linewidth, fmt=marker_style, color=LSST_passband_wavelengths_to_color[wavelength])
+            #ax.scatter(jd[idx], flux[idx], marker=marker_style, s=marker_size, color=LSST_passband_wavelengths_to_color[wavelength])
 
         # Save the figure as PNG with the desired DPI
         dpi = 100  # Dots per inch (adjust as needed)
-
-        # Set the figure size in inches
-        width_in = 2.56  # Desired width in inches (256 pixels / 100 dpi)
-        height_in = 2.56  # Desired height in inches (256 pixels / 100 dpi)
-
-        fig.set_size_inches(width_in, height_in)
 
         ax.set_xticks([])
         ax.set_yticks([])
@@ -173,112 +229,54 @@ def add_lc_plots(examples):
         im = Image.open(buf).convert('RGB')
         img_arr = np.array(im, dtype=np.float32)
         img_arr = np.permute_dims(img_arr, (2, 0, 1))
+        img_arr = torch.from_numpy(img_arr)
 
-        # Add image array to the list
-        lc_plots_array.append(img_arr)
-        
         # Close the buffer
         buf.close()
 
-    # Add plots of the light curves
-    examples['lc_plots'] = lc_plots_array
+        return img_arr
 
-    return examples
+def custom_collate(batch):
 
-def replace_labels(examples, mapper: dict):
+    batch_size = len(batch)
 
-    # Get the number of samples
-    N_samples = len(examples['SNID'])
+    ts_array = []
+    label_array = []
+    snid_array = np.zeros((batch_size))
 
-    for i in range(N_samples):
+    lengths = np.zeros((batch_size), dtype=np.float32)
+    static_features_tensor = torch.zeros((batch_size, n_static_features),  dtype=torch.float32)
+    lc_plot_tensor = torch.zeros((batch_size, n_channels, img_height, img_width), dtype=torch.float32)
 
-        examples['ELASTICC_class'][i] = mapper[examples['ELASTICC_class'][i]]
+    for i, sample in enumerate(batch):
 
-    return examples
+        ts_array.append(sample['ts'])
+        label_array.append(sample['label'])
 
-def collate_ELAsTiCC_lc_data(batch, includes_plots=True):
+        snid_array[i] = sample['SNID']
+        lengths[i] = sample['ts'].shape[0]
+        static_features_tensor[i,:] = sample['static']
 
-    ts_data = []
-    static_data = []
-    plots = []
-    labels = []
-    snids = []
-    lengths = []
+        if 'lc_plot' in sample.keys():
+            lc_plot_tensor[i,:,:,:] = sample['lc_plot']
 
-    for b in batch:
+    lengths = torch.from_numpy(lengths)
+    label_array = np.array(label_array)
 
-        length = len(b['MJD'])
-        n_ts_features = len(time_dependent_feature_list)
+    ts_tensor = pad_sequence(ts_array, batch_first=True, padding_value=flag_value)
 
-        # Fill the array with the time series data
-        array = np.zeros((length, n_ts_features), dtype=np.float32)
-        array[:, 0] = b['MJD']
-        array[:, 1] = b['FLUXCAL']
-        array[:, 2] = b['FLUXCALERR']
-        array[:, 3] = b['BAND']
-        array[:, 4] = b['PHOTFLAG']
-        ts_data.append(array)
-
-        # Fill the array with the static data
-        s = []
-        for feature in time_independent_feature_list:
-            s.append(b[feature])
-
-        static_data.append(np.array(s, dtype=np.float32))
-        lengths.append(length)
-        labels.append(b['ELASTICC_class'])
-        snids.append(b['SNID'])
-
-        if includes_plots:
-            plots.append(b['lc_plots'])
-
-        
-    ts_data = [torch.from_numpy(x) for x in ts_data]
-    ts_data = pad_sequence(ts_data, batch_first=True, padding_value=flag_value)
-
-    static_data = torch.from_numpy(np.array(static_data))
-    
     d = {
-        'ts_data':ts_data,
-        'static_data':static_data,
-        'lengths':lengths,
-        'labels':labels,
-        'SNID':snids
+        'ts': ts_tensor,
+        'static': static_features_tensor, 
+        'length': lengths,
+        'label': label_array,
+        'SNID': snid_array,
     }
 
-    if includes_plots:
-        plots = torch.from_numpy(np.array(plots))
-        d['lc_plots'] = plots
+    if 'lc_plot' in sample.keys():
+        d['lc_plot'] = lc_plot_tensor
 
     return d
-
-def get_ELAsTiCC_dataset(split, parquet_path=parquet_path):
-
-    # Load the whole dataset
-    dataset = load_dataset("parquet", data_files=parquet_path, split='train')
-
-    # Break the dataset into train, test, and val splits
-    seed = 42
-    ds_train_test = dataset.train_test_split(test_size=0.3, seed=seed)
-    ds_test_val = ds_train_test['train'].train_test_split(test_size=0.1, seed=seed)
-
-    # Create a new dataset dictionary
-    dataset_splits = DatasetDict({
-        'train': ds_test_val['train'],
-        'validation': ds_test_val['test'],
-        'test': ds_train_test['test']
-    })
-
-    # Select the correct split
-    dataset = dataset_splits[split]
-
-    # Only select the useful columns
-    dataset = dataset.select_columns(time_independent_feature_list+time_dependent_feature_list+book_keeping_feature_list)
-
-    # Set the appropriate formatting for all the data
-    dataset.set_format(type="torch")
-
-    return dataset
 
 def show_batch(images, labels, n=16):
 
@@ -306,58 +304,21 @@ def show_batch(images, labels, n=16):
     plt.tight_layout()
     plt.show()
 
-def main():
+if __name__=='__main__':
+    # <--- Example usage of the dataset --->
+    # dataset = BTS_LC_Image_Dataset('data/BTS/bts_lc_images')
+    # dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
-    dataset = get_ELAsTiCC_dataset('train')
+    # for i, item in enumerate(dataloader):
+    #     data, labels = item
+    #     print(data.shape)
+    #     print(labels)
+    #     break
 
-    # Create a custom function with all the transforms
-    def custom_transforms_function(examples):
+    dataset = ELAsTiCC_LC_Dataset('data/ELAsTiCC/test.parquet', include_lc_plots=False, transform=truncate_light_curve_fractionally)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, collate_fn=custom_collate)
 
-        # Mask off any saturations
-        examples = mask_off_saturations(examples)
-
-        # Replace any missing values with a flag
-        examples = replace_missing_value_flags(examples)
-
-        # Truncate LC's before building the plots
-        examples = truncate_lcs_fractionally(examples)
-
-        # Add plots of the light curve for the vision transformer
-        examples = add_lc_plots(examples)
-
-        # Convert the labels from ELAsTiCC labels to astrophysically meaningful labels
-        examples = replace_labels(examples, ELAsTiCC_to_Astrophysical_mappings)
-
-        return examples
-
-    dataset = dataset.with_transform(custom_transforms_function)
-    dataloader = DataLoader(dataset, batch_size=32, collate_fn=collate_ELAsTiCC_lc_data, shuffle=True)
-
-    taxonomy = ORACLE_Taxonomy()
-    model = ORACLE1(taxonomy)
-    model.eval()
-
-    model_VT = ORACLE2_lite_swin(taxonomy)
-    model_VT.eval()
-
-    for batch in dataloader:
-
-        print(list(batch.keys()))
-
-        logits = model(batch)
+    for batch in tqdm(dataloader):
+        pass
         
-        print(model.predict_class_probabilities_df(batch))
-        print(model_VT.predict_class_probabilities_df(batch))
 
-        print(logits.shape)
-        print(batch['lc_plots'].shape)
-        print(batch['ts_data'].shape)
-        print(batch['static_data'].shape)
-        print(batch['labels'])
-
-        show_batch(batch['lc_plots'], batch['labels'])
-        break
-
-if __name__ == '__main__':
-
-    main()

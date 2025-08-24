d35dcdd104cda671cbe04f33e76bc2d3879aaa97
ved <vedshah.email@gmail.com>
2025-08-24 14:00:04 -0500

    Updated class mappings

--- src/oracle/constants.py
+++ src/oracle/constants.py
@@ -1,48 +1,14 @@
-ELAsTiCC_to_Astrophysical_mappings = {
-    'SNII-NMF': 'SNII', 
-    'SNIc-Templates': 'SNIb/c', 
-    'CART': 'CART', 
-    'EB': 'EB', 
-    'SNIc+HostXT_V19': 'SNIb/c', 
-    'd-Sct': 'Delta Scuti', 
-    'SNIb-Templates': 'SNIb/c', 
-    'SNIIb+HostXT_V19': 'SNII', 
-    'SNIcBL+HostXT_V19': 'SNIb/c', 
-    'CLAGN': 'AGN', 
-    'PISN': 'PISN', 
-    'Cepheid': 'Cepheid', 
-    'TDE': 'TDE', 
-    'SNIa-91bg': 'SNI91bg', 
-    'SLSN-I+host': 'SLSN', 
-    'SNIIn-MOSFIT': 'SNII', 
-    'SNII+HostXT_V19': 'SNII', 
-    'SLSN-I_no_host': 'SLSN', 
-    'SNII-Templates': 'SNII', 
-    'SNIax': 'SNIax', 
-    'SNIa-SALT3': 'SNIa', 
-    'KN_K17': 'KN', 
-    'SNIIn+HostXT_V19': 'SNII', 
-    'dwarf-nova': 'Dwarf Novae', 
-    'uLens-Binary': 'uLens', 
-    'RRL': 'RR Lyrae', 
-    'Mdwarf-flare': 'M-dwarf Flare', 
-    'ILOT': 'ILOT', 
-    'KN_B19': 'KN', 
-    'uLens-Single-GenLens': 'uLens', 
-    'SNIb+HostXT_V19': 'SNIb/c', 
-    'uLens-Single_PyLIMA': 'uLens'
-}
-
 BTS_to_Astrophysical_mappings = {
     'AGN': 'AGN',
-    'AGN?': 'AGN',
+    'AGN?': 'Anomaly',
     'CLAGN': 'AGN',
-    'bogus?': 'Transient-Other',
-    'rock': 'Transient-Other',
+    'bogus?': 'Anomaly',
+    'rock': 'Anomaly',
     'CV': 'CV',
     'CV?': 'CV',
     'AM CVn': 'CV',
-    'varstar': 'Persistent-Other',
+    'varstar': 'Anomaly',
+    "Cataclysmic": 'CV',
     'QSO': 'AGN', # AGN?
     'QSO?': 'AGN', # AGN?
     'NLS1': 'AGN', # AGN?
@@ -52,14 +18,14 @@ BTS_to_Astrophysical_mappings = {
     'blazar': 'AGN', # AGN?
     'blazar?': 'AGN', # AGN?
     'Seyfert': 'AGN', # AGN?
-    'star': 'Persistent-Other',
-    'Ien': 'Persistent-Other',
-    'LINER': 'Persistent-Other',
-    'Ca-rich': 'Transient-Other', 
-    'FBOT': 'Transient-Other',
-    'ILRT': 'Transient-Other',
-    'LBV': 'Transient-Other',
-    'LRN': 'Transient-Other',
+    'star': 'Anomaly',
+    'Ien': 'Anomaly',
+    'LINER': 'Anomaly',
+    'Ca-rich': 'Anomaly', 
+    'FBOT': 'Anomaly',
+    'ILRT': 'Anomaly',
+    'LBV': 'Anomaly',
+    'LRN': 'Anomaly',
     'SLSN-I': 'SLSN',
     'SLSN-I.5': 'SLSN',
     'SLSN-I?': 'SLSN',
@@ -67,46 +33,48 @@ BTS_to_Astrophysical_mappings = {
     'SN II': 'SN-II',
     'SN II-SL': 'SLSN',
     'SN II-norm': 'SN-II',
-    'SN II-pec': 'SN-II',
+    'SN II-pec': 'Anomaly',
     'SN II?': 'SN-II',
     'SN IIL': 'SN-II',
     'SN IIP': 'SN-II',
-    'SN IIb': 'SN-II',
-    'SN IIb-pec': 'SN-II',
-    'SN IIb?': 'SN-II',
+    'SN IIb': 'Anomaly',
+    'SN IIb-pec': 'Anomaly',
+    'SN IIb?': 'Anomaly',
     'SN IIn': 'SN-II',
     'SN IIn?': 'SN-II',
     'SN Ia': 'SN-Ia',
-    'SN Ia-00cx': 'SN-Ia',# pec
-    'SN Ia-03fg': 'SN-Ia',# pec
+    'SN Ia-00cx': 'Anomaly',# pec
+    'SN Ia-03fg': 'Anomaly',# pec
     'SN Ia-91T': 'SN-Ia',
-    'SN Ia-91bg': 'SN-Ia',# pec
-    'SN Ia-91bg?': 'SN-Ia',# pec
-    'SN Ia-99aa': 'SN-Ia',
-    'SN Ia-CSM': 'SN-Ia',# pec
-    'SN Ia-CSM?': 'SN-Ia',# pec
+    'SN Ia-91bg': 'Anomaly',# pec
+    'SN Ia-91bg?': 'Anomaly',# pec
+    'SN Ia-99aa': 'Anomaly',
+    'SN Ia-CSM': 'Anomaly',# pec
+    'SN Ia-CSM?': 'Anomaly',# pec
     'SN Ia-norm': 'SN-Ia',
-    'SN Ia-pec': 'SN-Ia',# pec
-    'SN Ia?': 'SN-Ia',
-    'SN Iax': 'SN-Ia', # pec
+    'SN Ia-pec': 'Anomaly',# pec
+    'SN Ia?': 'Anomaly',
+    'SN Iax': 'Anomaly', # pec
     'SN Ib': 'SN-Ib/c',
-    'SN Ib-pec': 'SN-Ib/c',
+    'SN Ib-pec': 'Anomaly',
     'SN Ib/c': 'SN-Ib/c',
     'SN Ib/c?': 'SN-Ib/c',
     'SN Ib?': 'SN-Ib/c',
-    'SN Ibn': 'SN-Ib/c',
-    'SN Ibn?': 'SN-Ib/c',
+    'SN Ibn': 'Anomaly',
+    'SN Ibn?': 'Anomaly',
     'SN Ic': 'SN-Ib/c',
-    'SN Ic-BL': 'SN-Ib/c',
-    'SN Ic-BL?': 'SN-Ib/c',
-    'SN Ic-SL': 'SN-Ib/c',
+    'SN Ic-BL': 'Anomaly',
+    'SN Ic-BL?': 'Anomaly',
+    'SN Ic-SL': 'Anomaly',
     'SN Ic?': 'SN-Ib/c',
-    'SN Icn': 'SN-Ib/c',
-    'TDE': 'Transient-Other',
-    'afterglow': 'Transient-Other',
+    'SN Icn': 'Anomaly',
+    'TDE': 'Anomaly',
+    'afterglow': 'Anomaly',
     'nova': 'CV',
     'nova-like': 'CV',
     'nova?': 'CV',
+    "microlensing": 'Anomaly',
+    "SN-Ib/c": "SN-Ib/c",
 
 }
 
@@ -119,21 +87,21 @@ ZTF_sims_to_Astrophysical_mappings = {
     'SNCC-II': 'SN-II',  
     'SNIa-91bg': 'SN-Ia',   
     'SNIa-x ': 'SN-Ia',  
-    'KN': 'Transient-Other',  
-    'SLSN-I': 'SLSN',   
-    'PISN': 'Transient-Other',   
-    'ILOT': 'Transient-Other',    
-    'CART': 'Transient-Other',    
-    'TDE': 'Transient-Other',    
+    'KN': 'CV',  # TODO: Add CV class - no too worried about fine tuning
+    'SLSN-I': 'SLSN-I',   
+    'PISN': 'Anomaly',   
+    'ILOT': 'Anomaly',    
+    'CART': 'Anomaly',    
+    'TDE': 'Anomaly',    
     'AGN': 'AGN',    
-    'RRlyrae': 'Persistent-Other',   
+    'RRlyrae': 'Anomaly',   
     'Mdwarf': 'CV',    
-    'EBE': 'Persistent-Other',    
-    'MIRA': 'Persistent-Other',    
-    'uLens-Binary': 'Transient-Other',    
-    'uLens-Point': 'Transient-Other',    
-    'uLens-STRING': 'Transient-Other',    
-    'uLens-Point': 'Transient-Other',    
+    'EBE': 'Anomaly',    
+    'MIRA': 'Anomaly',    
+    'uLens-Binary': 'Anomaly',    
+    'uLens-Point': 'Anomaly',    
+    'uLens-STRING': 'Anomaly',    
+    'uLens-Point': 'Anomaly',    
 }
 
 ztf_fid_to_filter = {

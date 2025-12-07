bb7a26058106d3e7f6ddf6766ac9c99ff9f518f4
ved <vedshah.email@gmail.com>
2025-12-06 19:22:47 -0600

    Added time jitter

--- src/oracle/custom_datasets/BTS.py
+++ src/oracle/custom_datasets/BTS.py
@@ -723,7 +723,7 @@ def truncate_BTS_light_curve_by_days_since_trigger(x_ts, x_static, d=None, add_j
         
         # Jitter the time
         x_ts[:, jd_index] = x_ts[:, jd_index] + np.random.uniform([0]*x_ts.shape[0],  [0.25]*x_ts.shape[0])
-        x_ts[:, jd_index] -= min(x_ts[:, jd_index])
+        x_ts[:, jd_index] -= x_ts[:, jd_index].min().item()
 
     # Get indices of observations within d days of the first detection (trigger)
     idx = np.where(jd <= d)[0]

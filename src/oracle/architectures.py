8e3f1eaf0ca84779e3870c4c669e5912b703b3c0
ved <vedshah.email@gmail.com>
2025-12-08 16:09:32 -0600

    Updating models that are used for the MM

--- src/oracle/architectures.py
+++ src/oracle/architectures.py
@@ -717,8 +717,8 @@ class ConvNeXt(Hierarchical_classifier):
 class GRU_MD_MM_Improved(Hierarchical_classifier):
 
     def __init__(self, taxonomy: Taxonomy,
-                 lc_md_model_dir="models/BTSv2/frosty-oath-206/",
-                 image_model_dir="models/BTSv2_PSonly/solar-microwave-238/"):
+                 lc_md_model_dir="models/BTSv2/crisp-lake-253/",
+                 image_model_dir="models/BTSv2_PSonly/snowy-hill-244/"):
         
         super().__init__(taxonomy)
 

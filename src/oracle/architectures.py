263a24cff105b4fade26e9f439f5fd47882ab541
ved <vedshah.email@gmail.com>
2025-12-15 17:32:36 -0600

    freezing most of the model for mallorn

--- src/oracle/architectures.py
+++ src/oracle/architectures.py
@@ -725,11 +725,12 @@ class GRU_MALLORN(Hierarchical_classifier):
         self.base_model = GRU(ORACLE_Taxonomy())
         self.base_model.load_state_dict(torch.load(f'{base_model_dir}/best_model_f1.pth', map_location=torch.device('cpu')), strict=False)
         for p in self.base_model.parameters():
-            p.requires_grad = True
+            p.requires_grad = False
 
         self.final_out = nn.Sequential(
-            nn.GELU(),
-            nn.Linear(16, self.output_dim),
+            nn.GELU(), nn.Linear(16, self.output_dim),
+            nn.GELU(), nn.Linear(8, self.output_dim),
+            nn.GELU(), nn.Linear(4, self.output_dim),
         )
 
     def get_latent_space_embeddings(self, batch):

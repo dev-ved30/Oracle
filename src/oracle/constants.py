37f365da070bbd5731e973230a386858d489cc59
ved <vedshah.email@gmail.com>
2026-01-20 02:19:56 -0600

    Added SN-IIn to the BTS taxonomy

--- src/oracle/constants.py
+++ src/oracle/constants.py
@@ -78,8 +78,8 @@ BTS_to_Astrophysical_mappings = {
     'SN IIb': 'SN-II',
     'SN IIb-pec': 'SN-II',
     'SN IIb?': 'SN-II',
-    'SN IIn': 'SN-II',
-    'SN IIn?': 'SN-II',
+    'SN IIn': 'SN-IIn',
+    'SN IIn?': 'SN-IIn',
     'SN Ia': 'SN-Ia',
     'SN Ia-00cx': 'SN-Ia',# pec
     'SN Ia-03fg': 'SN-Ia',# pec
@@ -125,7 +125,7 @@ BTS_to_Astrophysical_mappings_AD = {
     'CV': 'CV',
     'CV?': 'CV',
     'AM CVn': 'CV',
-    'varstar': 'Anomaly',
+    'varstar': 'Varstar',
     "Cataclysmic": 'CV',
     'QSO': 'AGN', # AGN?
     'QSO?': 'AGN', # AGN?
@@ -158,8 +158,8 @@ BTS_to_Astrophysical_mappings_AD = {
     'SN IIb': 'SN-II',
     'SN IIb-pec': 'SN-II',
     'SN IIb?': 'SN-II',
-    'SN IIn': 'SN-II',
-    'SN IIn?': 'SN-II',
+    'SN IIn': 'SN-IIn',
+    'SN IIn?': 'SN-IIn',
     'SN Ia': 'SN-Ia',
     'SN Ia-00cx': 'Anomaly',# pec
     'SN Ia-03fg': 'Anomaly',# pec
@@ -183,7 +183,7 @@ BTS_to_Astrophysical_mappings_AD = {
     'SN Ic': 'SN-Ib/c',
     'SN Ic-BL': 'Anomaly',
     'SN Ic-BL?': 'Anomaly',
-    'SN Ic-SL': 'SLSN',
+    'SN Ic-SL': 'Anomaly',
     'SN Ic?': 'SN-Ib/c',
     'SN Icn': 'Anomaly',
     'TDE': 'Anomaly',

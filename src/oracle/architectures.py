6a5e2289de4212ed9d69f0200d01f1e7c80e31a8
ved <vedshah.email@gmail.com>
2026-02-20 18:04:57 -0600

    Using the best new pretrained models as backbones for the omni model

--- src/oracle/architectures.py
+++ src/oracle/architectures.py
@@ -752,8 +752,8 @@ class GRU_MALLORN(Hierarchical_classifier):
 class GRU_MD_MM_Improved(Hierarchical_classifier):
 
     def __init__(self, taxonomy: Taxonomy,
-                 lc_md_model_dir="models/BTSv2/avid-brook-349/",
-                 image_model_dir="models/BTSv2_PSonly/polar-rain-350/"):
+                 lc_md_model_dir="models/BTSv2/peachy-sweep-4/",
+                 image_model_dir="models/BTSv2_PSonly/stoic-sweep-6/"):
         
         super().__init__(taxonomy)
 

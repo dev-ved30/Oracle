576e1c3e80c435e1f89004c9e6316cac8e82fa87
ved <vedshah.email@gmail.com>
2026-04-15 10:45:46 -0500

    updated paths to use new dataset files with ztf refs

--- src/oracle/custom_datasets/BTS.py
+++ src/oracle/custom_datasets/BTS.py
@@ -24,8 +24,8 @@ from oracle.constants import ztf_filters, ztf_alert_image_order, ztf_alert_image
 here = Path(__file__).resolve().parent
 
 # Go up to the root, then into data/ and then get the parquet file
-BTS_train_parquet_path = str(here.parent.parent.parent / "data" / 'BTSv3' / 'train_PS.parquet')
-BTS_test_parquet_path = str(here.parent.parent.parent / "data" / 'BTSv3' / 'test_PS.parquet')
+BTS_train_parquet_path = str(here.parent.parent.parent / "data" / 'BTSv3' / 'train_PS_ZTF.parquet')
+BTS_test_parquet_path = str(here.parent.parent.parent / "data" / 'BTSv3' / 'test_PS_ZTF.parquet')
 BTS_val_parquet_path = str(here.parent.parent.parent / "data" / 'BTSv3' / 'val_PS_ZTF.parquet')
 
 # <----- constant for the dataset ----->
